614a3f27b9a107dd2a4ccfcc220343b809ee424c
KAMAL <147421502+sachin3270@users.noreply.github.com>
2024-03-14 21:27:42 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 const { createClient } = supabase;
   
 document.addEventListener('DOMContentLoaded', async function () {
   const supabaseUrl = 'https://ettldmnlhlytrjavmpmt.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0dGxkbW5saGx5dHJqYXZtcG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5OTYxNTAsImV4cCI6MjAyMjU3MjE1MH0.J9YoAtB7BHe6t9t8do0zlLHp9468akRJ3NQb4PcwcPk'; // Set your Supabase key here
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0dGxkbW5saGx5dHJqYXZtcG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5OTYxNTAsImV4cCI6MjAyMjU3MjE1MH0.J9YoAtB7BHe6t9t8do0zlLHp9468akRJ3NQb4PcwcPk';
 
   const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -34,32 +33,32 @@ document.addEventListener('DOMContentLoaded', async function () {
 
       const hashed_pwd = CryptoJS.MD5(password)
 
-      
-
       for (var i = 0; i < data.length; i++)
       {
         if (data[i].userid == username)
         {
-          if (data[i].hashed_password == hashed_pwd)
+          if (data[i].password_hash == hashed_pwd)
           {
             if (data[i].role == "admin")
             {
-              console.log(data);
+              window.location.href = 'admin.html';
+              return;
             }
             else
             {
-              console.log(data[i]);
+              var toDisplay = JSON.stringify(data[i],null, 2);
+              alert(toDisplay);
             }
             return;
           }
           else
           {
-            console.log("Incorrect Password");
+            alert("Incorrect Password");
             return;
           }
         }
       }
-      console.log("User does not exist");
+      alert("User does not exist");
     }
      catch (error) {
       console.error(error.message);
